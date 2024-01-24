b7e5d96a8127e861fcc2d6049d74ee82054cf275
slymario <nwobodo_obinna@yahoo.com>
2024-01-24 11:09:03 +0100

    new files added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,60 @@
 import React from "react";
 import "./Portfolio.css"
+import IMG1 from "../../assets/portfolio1.png"
+import IMG2 from "../../assets/portfolio2.png"
+import IMG3 from "../../assets/portfolio3.png"
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Elis Fragrance Website",
+    github: "https://github.com",
+    demo: "https://dribbble.com/clickable_design"
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Blog Web App",
+    github: "https://github.com",
+    demo: "https://dribbble.com/clickable_design"
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Fodd Delivery Landing Page",
+    github: "https://github.com",
+    demo: "https://dribbble.com/clickable_design"
+  },
+]
+
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github,demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn" target="blank">Github</a>
+                  <a href={demo} className="btn btn-primary" target="blank">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,80 @@
 import React from "react";
 import "./Services.css"
+import { FaCheck } from "react-icons/fa";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>User Experience and User Interface</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Custom Web Application Development</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Single Page Application (SPA) Development</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Responsive Web Design</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>User Authentication and Authorization</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Intuitive Navigation Design</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Visual Design Consistency</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX */}
+        <article className="service">
+          <div className="service__head">
+            <h3>System Architecture and Functionality</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Full-Stack Development</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>RESTful API Development</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Database Design and Integration</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Deployment and Hosting</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Version Control and Collaboration</p>
+            </li>
+            <li>
+              <IoMdCheckmarkCircleOutline className="list__icon"/>
+              <p>Consultation and Project Planning</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
