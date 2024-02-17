db6a2c67b0c5a96f15698d344348de9150c04fdf
slymario <nwobodo_obinna@yahoo.com>
2024-02-17 11:31:29 +0100

    New files created

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,10 +1,89 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import "./Contact.css";
+import emailjs from "@emailjs/browser";
+import toast, {Toaster} from 'react-hot-toast';
+import { MdOutlineMailOutline } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { FaWhatsapp } from "react-icons/fa";
+import { RiCheckLine } from "react-icons/ri";
 
 const Contact = () => {
+  const [messageSent, setMessageSent] = useState(false); // State to track success state
+
+  const email = 'nwobodo_obinna@yahoo.com';
+  const encodedEmail = encodeURIComponent(email);
+
+  const notify = () => toast.success('Message sent successfully.');
+  const form = useRef();
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    try {
+      const result = await emailjs.sendForm("service_vm8n5fd", "template_lfag344", form.current, "aiFPsR2KMexmIhex2");
+      console.log(result.text);
+      setMessageSent(true);
+      notify();
+      e.target.reset();
+    } catch (error) {
+      console.log(error.text);
+    }
+  };
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMailOutline className="contact__icon" />
+            <h4>Email</h4>
+            <h5>{email}</h5>
+            <a href={`mailto:${encodedEmail}`}>Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <FaWhatsapp className="contact__icon"/>
+            <h4>Whatsapp</h4>
+            <h5>+234 806 818 6317</h5>
+            <a href="https://wa.me/2348068186317" target="_blank">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsMessenger className="contact__icon" />
+            <h4>Messanger</h4>
+            <h5>Nwobodo Mario Obinna</h5>
+            <a href="https://m.me/nwobodo1" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+
+        <form ref={form} onSubmit={sendEmail}>
+
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="text" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Enter Your Message" required ></textarea>
+          <button type="submit" className="btn btn-primary" onClick={notify}>Send Message</button>
+          
+          {messageSent && (
+            <div className="success-icon">
+              <RiCheckLine color="green" size={30} />
+            </div>
+          )}
+
+          <Toaster containerStyle={{
+                    top: 50,
+                    left: 50,
+                    bottom: 60,
+                    right: 60,
+                }} />
+
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default Contact
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import "./Footer.css"
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">NWOBODO OBINNA</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li> 
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com/nwobodoo1" target="_blank" rel="noopener noreferrer"><FaFacebookF/></a>
+        <a href="https://instagram.com/mistamarioo" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+        <a href="https://twitter.com/sly_mario" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>Copyright &copy; Nwobodo Obinna.</small>
+      </div>
+    </footer>
   )
 }
 
