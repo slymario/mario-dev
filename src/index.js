43e7fc40403bd9dbe7468a4417b827604c0aec58
slymario <nwobodo_obinna@yahoo.com>
2024-06-14 06:55:16 +0100

    Changes made in the indexjs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+root.render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>,
-    document.querySelector("#root")
-    );
+    </BrowserRouter>
+);
+
