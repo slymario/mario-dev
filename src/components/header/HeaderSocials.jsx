d04d512445f424c5ce9a63e1308d2b56cd0b52e3
slymario <nwobodo_obinna@yahoo.com>
2024-06-16 10:13:42 +0100

    Header socials updated

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import { FaGithub } from "react-icons/fa";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"  rel="noopener noreferrer"><BsLinkedin /></a>
-        <a href="https://github.com" target="_blank"  rel="noopener noreferrer"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/obinna-nwobodo-24aa6717b/" target="_blank"  rel="noopener noreferrer"><BsLinkedin /></a>
+        <a href="https://github.com/slymario" target="_blank"  rel="noopener noreferrer"><FaGithub /></a>
     </div>
   )
 }
